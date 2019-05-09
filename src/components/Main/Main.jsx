83986191f504415db5063d2caf4a0f7c36a449bd
Michael KS <kingstockton@gmail.com>
2019-05-09 14:49:37 -0600

    card container displays movies on the dom

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,12 +4,6 @@ import Footer from "../Footer/Footer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "../Home/Home";
 import SearchResults from "../SearchResults/SearchResults";
-import { APIkey } from "../../Utility/Config/Key";
-import { getTopMovies } from "../../Utility/Helpers/GetTopMovies";
-import { getTopTVShows } from "../../Utility/Helpers/GetTopTVShows";
-import { connect } from "react-redux";
-import { getMovies } from '../../Utility/Helpers/GetMovies';
-import {addTopMovies, addTopTVShows} from "../../Actions/index";
 import UserSignIn from '../UserSignIn/UserSignIn';
 
 class Main extends Component {
@@ -25,16 +19,17 @@ class Main extends Component {
   }
 
   async componentDidMount() {
-    const { currentPage, } = this.state;
-    let incrementedPage = currentPage + 1;
-    let movies = await getTopMovies(APIkey, incrementedPage);
-    this.props.addTopMovies(movies)
-    let topShows = await getTopTVShows(APIkey, incrementedPage);
-    this.props.addTopTVShows(topShows)
-    this.setState({currentPage: incrementedPage})
 
-    // selects a random number - can be used to pick from either array above and display banner
-    // const randomNumber = Math.floor(Math.random() * 20) + 1;
+
+
+
+
+
+
+
+
+
+
   }
 
   handleFilterClick = criteria => {
@@ -56,16 +51,6 @@ class Main extends Component {
   }
 }
 
-const mapStateToProps = state => ({
-  topMovies : state.topMovies
-});
 
-const mapDispatchToProps = dispatch => ({
-  addTopMovies : movies => dispatch(addTopMovies(movies)),
-  addTopTVShows: topShows => dispatch(addTopTVShows(topShows))
-});
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Main);
+export default Main
