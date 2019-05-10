4b49efab19ae93b6b85cc0ecc0f9198226c1b511
Max Silver <Maxbsilver@gmail.com>
2019-05-10 12:29:24 -0600

    nav-styling

--- src/Containers/Nav/Nav.jsx
+++ src/Containers/Nav/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
-import {removeUser} from '../../Actions'
+import { removeUser } from "../../Actions";
 
 class Nav extends Component {
   constructor(props) {
@@ -14,12 +14,19 @@ class Nav extends Component {
     return this.props.user.name === undefined ? (
       <nav>
         <ul>
+          <div className="main-nav-container">
+          <h1 className="nav-title">Movie Tracker</h1>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
           </li>
+          </div>
           <section className="sign-in-container" />
           <li>
-            <Link to="/login">Log in/Sign up</Link>
+            <NavLink to="/login" className="nav-link">
+              Log in/Sign up
+            </NavLink>
           </li>
         </ul>
       </nav>
@@ -27,11 +34,18 @@ class Nav extends Component {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
           </li>
           <section className="sign-in-container" />
           <li>
-            <button onClick = {() => this.props.removeUser({})}>Sign Out</button >
+            <button
+              className="nav-link"
+              onClick={() => this.props.removeUser({})}
+            >
+              Sign Out
+            </button>
           </li>
         </ul>
       </nav>
@@ -44,6 +58,9 @@ const mapStateToProps = state => ({
 });
 const mapDispatchToProps = dispatch => ({
   removeUser: user => dispatch(removeUser(user))
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Nav);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Nav);
