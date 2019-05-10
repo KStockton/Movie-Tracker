7ad6b246f1b89c8816677c11e2a2312633191636
Michael KS <kingstockton@gmail.com>
2019-05-09 20:54:10 -0600

    fix to favorite/nofavorite image load

--- src/Utility/Cleaners/CleanTopMovies.js
+++ src/Utility/Cleaners/CleanTopMovies.js
@@ -11,7 +11,7 @@ const films = topMovies.map(topMovie => {
     overview: topMovie.overview,
     posterpath: topMovie.poster_path,
     popularity: topMovie.vote_average,
-    favorite: false
+    favorite: true
   }
 })
 return films
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
+import noFav from '../../Images/noFav.svg'
+import fav from '../../Images/fav.svg'
 
 const  Card = ({movie})  => {
   console.log(movie)
-  const {id, title, overview, image, favorite } = movie
-  const imagesrc = `https://image.tmdb.org/t/p/w500${image}`
-  let style = {
-      backgroundImage: `url(${imagesrc})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center'
-  }
+  const {id,overview, popularity, favorite, posterpath } = movie
+  console.log(movie)
+  const imagesrc = `https://image.tmdb.org/t/p/w500${posterpath}`
 
   return (
-    <div className="Card-wrapper" style={style}>
-    {title && <p>Title: {title}</p>}
-    {overview && <p>Overview {overview}</p>}
+    <div className="Card-wrapper" >
+    {popularity && <p className="Card-info">Overview {popularity}</p>}
+    <img className="Card-no-favorite" src={favorite ? fav : noFav} alt="favorite"/>
+    <img src={imagesrc} alt="poster"/>
+    <button>Favorite</button>
     </div>
   )
 }
