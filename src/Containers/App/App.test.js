aafda9d9f2de8a3358f05b83edbd801fff7a3f7d
Michael KS <kingstockton@gmail.com>
2019-05-13 18:42:56 -0600

    add App Test and Main test

--- src/Containers/App/App.test.js
+++ src/Containers/App/App.test.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import App from './App';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+
+const mockStore = configureMockStore();
+const store = mockStore({});
 
 describe('App', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow (<App/>)
+    wrapper = shallow (
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    )
   })
 
-  it('App component should match the snapshot', () =>{
+  it('App component should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot()
   })
-
-
 })
-
-
-
-
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
