939d8939bcfdbb33961be3f654e17bbbb4b8fa82
Michael KS <kingstockton@gmail.com>
2019-05-14 15:04:42 -0600

    fixed test issue with store and provider

--- src/Containers/Home/Home.test.js
+++ src/Containers/Home/Home.test.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import Home from './Home';
+import { Home } from './Home';
 import { shallow } from 'enzyme';
-import { Provider } from "react-redux";
-import configureMockStore from "redux-mock-store";
 
-const mockStore = configureMockStore();
-const store = mockStore({});
 
 describe('Home', () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallow (
-    <Provider store={store}>
       <Home/>
-    </Provider>
-    ).shallow()
+    )
   })
 
   it('Home component should match the snapshot', () =>{
