c529289c2a78d43fb0a6c26e99475e7fe99beada
Max Silver <Maxbsilver@gmail.com>
2019-05-14 17:12:19 -0600

    testing-patch-1

--- src/Containers/UserSignIn/UserSignIn.test.js
+++ src/Containers/UserSignIn/UserSignIn.test.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import UserSignin from './UserSignin';
 import { shallow } from 'enzyme';
-import { Provider } from "react-redux";
-import configureMockStore from "redux-mock-store";
-
-const mockStore = configureMockStore();
-const store = mockStore({});
 
 describe('UserSignin', () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallow (
-      <Provider store={store}>
-        <UserSignin/>
-      </Provider>
+        <UserSignin />
     )
   })
 
