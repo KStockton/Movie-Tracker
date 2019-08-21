cb3a047baa225e754425fd02be60169a1a1745d7
Michael KS <kingstockton@gmail.com>
2019-08-20 18:25:24 -0600

    change folder name

--- src/Utility/Helpers/GetTopMovies.js
+++ src/Utility/Helpers/GetTopMovies.js
@@ -4,7 +4,7 @@ import { baseUrl } from '../../Utility/Config/BaseUrl';
 
 
 export const getTopMovies = async () => {
-  let url = `${baseUrl}movie/now_playing?api_key=${process.env.REACT_API_MOVIEDB}&language=en-US&page=2&region=US`;
+  let url = `${baseUrl}movie/now_playing?api_key=${process.env.REACT_API_MOVIEDB}&language=en-US&page=1&region=US`;
   const data = await fetchData(url);
   const result = await cleanTopMovies(data.results);
   return result;
